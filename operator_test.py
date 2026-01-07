31ddd1783a70557fedfa6d3c50a89eae9daafbcc
Demetre Seturidze <112725199+demes25@users.noreply.github.com>
2026-01-07 11:50:15 -0500

    Update operator_test.py
    updated operator test to comply with newest backend.

--- operator_test.py
+++ operator_test.py
@@ -4,8 +4,9 @@
 # Operator Test
 
 from operators import *
+from distributions import Distribute
 from geometry import Euclidean
-from lattices import Domain, Image 
+from lattices import Domain 
 from plots import save_plot
 
 # we test our current abilities.
@@ -20,20 +21,19 @@ end = 10.0
 # we want each point to look like [B, 1]
 dX = tf.constant([step])
 ranges = tf.constant([[start], [end]])
-domain = Domain(Euclidean(1), ranges, dX) # this is now [b, b, 1])
+domain = Domain(Euclidean(1), ranges, dX, padding=2) # this is now [b, b, 1])
 
-base_image = Image(domain, pad=2)
-base_mesh = tf.squeeze(base_image.view())
+base_mesh = tf.squeeze(domain.view())
 
-square_image = base_image.apply(tf.square, mutable=True)
+square_image = Distribute(tf.square, normalize=False)(domain)
 square_mesh = tf.squeeze(square_image.view())
 
 grad = Gradient(square_image)
 grad_mesh = tf.squeeze(grad.view())
 
-laplace = FlatLaplacian(square_image)
+laplace = FlatSpatialLaplacian(square_image)
 laplace_mesh = tf.squeeze(laplace.view())
 
 result = tf.stack([square_mesh, grad_mesh, laplace_mesh], axis=1)
 
-save_plot(base_mesh, result, 'square_plot.png')
+save_plot(base_mesh, result, 'square_plot_2.png')
